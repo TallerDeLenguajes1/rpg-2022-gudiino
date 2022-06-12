daa086029c8ff7538611b7c514aba3898cb981fc
gudiino <gudino1387@gmail.com>
2022-06-12 13:44:08 -0300

    carga archivo ignore, readme, creacion personaje

---
+++ personajes.cs
@@ -0,0 +1,20 @@
+using System;
+public class Personaje
+{
+    public class datos{
+        public string Tipo;
+        public string Nombre;
+        public string Apodo;
+        public DateTime FecNac;
+        public int Edad;
+        public float Salud;
+    }
+    public class Caracteristicas{
+        public int Velocidad;//1 a 10
+        public int Destreza;//1 a 5
+        public int Fuerza;//1 a 10
+        public int Nivel;//1 a 10
+        public int Armadura;//1 a 10
+    }
+
+}
