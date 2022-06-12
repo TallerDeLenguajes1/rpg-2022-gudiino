b17d54e410cc2e1ff1f5dd422552c942b5b8875b
gudiino <gudino1387@gmail.com>
2022-06-12 20:07:54 -0300

    inicializacion de datos y caracteristicas jugador

--- personajes.cs
+++ personajes.cs
@@ -1,20 +1,40 @@
 using System;
 public class Personaje
 {
-    public class datos{
-        public string Tipo;
-        public string Nombre;
-        public string Apodo;
-        public DateTime FecNac;
-        public int Edad;
-        public float Salud;
+    //DATOS
+    public int Tipo;
+    public enum Tipos{SoldadoModerno,GerreroMediaval,SoldadoInterestelar,Alien};
+    public string Nombre;
+    public int Apodo;
+    public enum Apodos{Destructor,Malote,Viejo,Fantasma,Nerd};
+    public DateTime FecNac;
+    public int Edad;//0 a 300
+    public float Salud;//100
+    //CARACTERISTICAS
+    public int Velocidad;//1 a 10
+    public int Destreza;//1 a 5
+    public int Fuerza;//1 a 10
+    public int Nivel;//1 a 10
+    public int Armadura;//1 a 10
+    public Personaje(int tipoo, string nomm, int apodoo, DateTime nace){
+        Tipo=tipoo;
+        Nombre=nomm;
+        Apodo=apodoo;
+        FecNac=nace;
     }
-    public class Caracteristicas{
-        public int Velocidad;//1 a 10
-        public int Destreza;//1 a 5
-        public int Fuerza;//1 a 10
-        public int Nivel;//1 a 10
-        public int Armadura;//1 a 10
+    public int Edad2()
+    {
+        DateTime actual = DateTime.Now;
+        var edad = actual - FecNac;
+        int anios = (int)(edad.TotalDays / 365.25);
+        return anios;
     }
-
+    public Personajes(){
+        Random numRan= new Random();
+        Velocidad=numRan.Next(1,10);
+        Destreza=numRan.Next(1,5);
+        Fuerza=numRan.Next(1,10);
+        Nivel=numRan.Next(1,10);
+        Armadura=numRan.Next(1,10);
+    }
 }
