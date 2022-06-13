9a0ee670a16385d7dc32b611e5e93aa3035ad57d
gudiino <gudino1387@gmail.com>
2022-06-12 21:07:46 -0300

    Muestra datos Personaje

--- personajes.cs
+++ personajes.cs
@@ -4,7 +4,8 @@ public class Personaje
     //DATOS
     public int Tipo;
     public enum Tipos{SoldadoModerno,GerreroMediaval,SoldadoInterestelar,Alien};
-    public string Nombre;
+    public int Nombre;
+    public enum Nombres{Jorge, Juan, Mario, Luciana, Marcos};
     public int Apodo;
     public enum Apodos{Destructor,Malote,Viejo,Fantasma,Nerd};
     public DateTime FecNac;
@@ -16,11 +17,12 @@ public class Personaje
     public int Fuerza;//1 a 10
     public int Nivel;//1 a 10
     public int Armadura;//1 a 10
-    public Personaje(int tipoo, string nomm, int apodoo, DateTime nace){
+    public Personaje(int tipoo, int nomm, int apodoo, DateTime nace){
         Tipo=tipoo;
         Nombre=nomm;
         Apodo=apodoo;
         FecNac=nace;
+        Salud=100;
     }
     public int Edad2()
     {
@@ -29,12 +31,21 @@ public class Personaje
         int anios = (int)(edad.TotalDays / 365.25);
         return anios;
     }
-    public Personajes(){
+    public Personaje(){
         Random numRan= new Random();
         Velocidad=numRan.Next(1,10);
         Destreza=numRan.Next(1,5);
         Fuerza=numRan.Next(1,10);
         Nivel=numRan.Next(1,10);
         Armadura=numRan.Next(1,10);
+    }
+    public void MuestraDatos(){
+        Console.WriteLine("Datos del Personaje");
+        Console.WriteLine("Tipo: {0}", Enum.GetName(typeof(Tipos), Tipo));
+        Console.WriteLine("Nombre: {0}", Enum.GetName(typeof(Nombres), Nombre));
+        Console.WriteLine("Apodo: {0}", Enum.GetName(typeof(Apodos), Apodo));
+        Console.WriteLine("Fecha de nacimiento: {0:d}", FecNac);
+        Console.WriteLine("Edad: "+Edad2()+" años");
+        Console.WriteLine("Salud: "+Salud);
     }
 }
